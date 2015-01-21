c96f32d4604083dc7db807a0fd479e3dfa17f6ef
hunterdavidsmith <huntersmith200@gmail.com>
2015-01-21 09:19:23 -0800

    finished video 6

--- js/screens/play.js
+++ js/screens/play.js
@@ -6,8 +6,10 @@ game.PlayScreen = me.ScreenObject.extend({
 		// reset the score
 		game.data.score = 0;
   	//be carefull when using caps thats is where most error are happening
-        me.levelDirector.loadLevel("level01")
+        me.levelDirector.loadLevel("level01");
 
+         var player = me.pool.pull("player", 0, 420, {});
+         me.game.world.addChild(player, 5);
 		// add our HUD to the game world
 		this.HUD = new game.HUD.Container();
 		me.game.world.addChild(this.HUD);
