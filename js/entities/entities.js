917e60e6fa29669de8ba5cca52882faa0c5b1801
hunterdavidsmith <huntersmith200@gmail.com>
2015-01-27 09:09:27 -0800

    finsishe dvideos 7-10

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -10,9 +10,26 @@
             	return(new me.Rect(0, 0, 64, 64)).toPolygon ();
             }
         }]);
+
+        this.body.setVelocity(5, 20);    
+
+   		this.renderable.addAnimation("walk, [117, 118, 119]")
+
  },
 
- update: function (){
+ update: function (delta){
+         if(me.input.isKeyPressed("right")) {
+ 		 
+ 			//sets the position of my x by adding the velocity defined above
+ 			//setVelocity() and multiplying it by me .timer.tick.
+ 			//me.timer.tick makes movement look smooth
+         	 this.body.vel.x += this.body.accel.x * me.timer.tick;
+          }else{
+          	this.body.vel.x = 0;
+          }
 
+
+          this.body.update (delta);
+          return true;
    }
 });
