05dee921a9e9bdad72e7e31b66b9a0dc5ffc3c24
hunterdavidsmith <huntersmith200@gmail.com>
2015-02-09 10:43:35 -0800

    finished 17

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -97,9 +97,17 @@
     	    var xdif = this.pos.x - response.b.pos.x;
     
     console.log("xdif " + xdif + "ydif " + ydif);
-         if (xdif>-35) {
+         
+          if (ydif<-40 && xdif< 70 && xdif>-35){
+ 		      this.body.falling = false;
+ 		      this.body.vel.y = -1;
+            }
+        else if (xdif>-35 && this.facing==='right' && (xdif<0)) {
          	 this.body.vel.x = 0;
          	 this.pos.x = this.pos.x -1;
+            }else if(xdif<70 && this.facing==='left' && xdif>0){
+             this.body.vel.x = 0;
+             this.pos.x = this.pos.x +1;
             }
          }
 
